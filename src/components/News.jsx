40935b1ea0749948b3b1a865b1b0e2ac4cd14102
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-01 19:47:31 +0100

    React app development

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -14,22 +14,27 @@ export const News = React.memo(() => {
     const {t} = useTranslation();
 
     const formHandle = (eo) => {
-        setEmailInfo({...emailInfo, [eo.target.name]: eo.target.value})
+        setEmailInfo({...emailInfo, [eo.target.name]: eo.target.value});
     }
 
 
     const sendInfoToServer = (eo) => {
         eo.preventDefault();
         yankiEvents.emit("startPostRequestWithEmail", emailInfo);
+        setEmailInfo({
+            from_name: '',
+            question: '',
+            from_email: ''
+        });
     }
 
     return (
         <div className="News">
             <h2>{t("about-new")}</h2>
             <form>
-                <input type="text" name="from_name" onChange={formHandle}/>
-                <input type="text" name="question" onChange={formHandle}/>
-                <input type="text" name="from_email" onChange={formHandle}/>
+                <input type="text" name="from_name" value={emailInfo.from_name} onChange={formHandle}/>
+                <input type="text" name="question" value={emailInfo.question} onChange={formHandle}/>
+                <input type="text" name="from_email" value={emailInfo.from_email} onChange={formHandle}/>
                 <button onClick={sendInfoToServer} type="button">Send</button>
             </form>
         </div>
