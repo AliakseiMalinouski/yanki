fce74cca487690cdb923753bbc2b4fb5c1e8f807
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-01 20:57:56 +0100

    React app development

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { yankiEvents } from "../events";
 
@@ -9,14 +9,18 @@ export const News = React.memo(() => {
         from_name: '',
         question: '',
         from_email: ''
-    })
+    });
+
+    const textarea = useRef(); 
+
+    const [currentLengthTextArea, setCurrentLengthTextArea] = useState(70);
 
     const {t} = useTranslation();
 
     const formHandle = (eo) => {
         setEmailInfo({...emailInfo, [eo.target.name]: eo.target.value});
+        if(eo.target.name === 'question') setCurrentLengthTextArea(prev => prev - 1);
     }
-
 
     const sendInfoToServer = (eo) => {
         eo.preventDefault();
@@ -31,11 +35,12 @@ export const News = React.memo(() => {
     return (
         <div className="News">
             <h2>{t("about-new")}</h2>
-            <form>
-                <input type="text" name="from_name" value={emailInfo.from_name} onChange={formHandle}/>
-                <input type="text" name="question" value={emailInfo.question} onChange={formHandle}/>
-                <input type="text" name="from_email" value={emailInfo.from_email} onChange={formHandle}/>
-                <button onClick={sendInfoToServer} type="button">Send</button>
+            <form className="SubscribeForm">
+                <input type="text" name="from_name" value={emailInfo.from_name} onChange={formHandle} placeholder={t("name-author-email")}/>
+                <textarea ref={textarea} maxLength={70} type="text" name="question" value={emailInfo.question} onChange={formHandle} placeholder={t("author-body-email")}></textarea>
+                <span>{currentLengthTextArea}</span>
+                <input type="text" name="from_email" value={emailInfo.from_email} onChange={formHandle} placeholder={t("author-email")}/>
+                <button onClick={sendInfoToServer} type="button">{t("subscribe-button-text")}</button>
             </form>
         </div>
     )
