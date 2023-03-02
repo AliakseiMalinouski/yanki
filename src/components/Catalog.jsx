4c9cc2ba9125dfcbf8c9872dcf6219dbf91c9bb5
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-02 16:13:11 +0100

    React app development

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useRef, useEffect } from "react";
+import { scrollToElement } from "../helpers/scroll";
 
 export const Catalog = () => {
+
+    let parentNode = useRef();
+
+    useEffect(() => {
+        scrollToElement(parentNode.current);
+    }, [parentNode]);
+
     return (
-        <div className="Catalog">
+        <div className="Catalog" ref={parentNode}>
             Catalog
         </div>
     )
