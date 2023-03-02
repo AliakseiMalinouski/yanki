1f920fdbe6f66363340f08a96cc3e5edf798e6d6
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-02 17:04:20 +0100

    React app development

--- src/Redux/Footer/footerContentThunk.js
+++ src/Redux/Footer/footerContentThunk.js
@@ -1,7 +1,7 @@
 import { setContentFooter } from "./footerContentSlice";
 
 export const footerContentThunk = (dispatch) => {
-    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/cfcfef95fc759cc4aebc5dcdb646616b/raw/e6d904a2adf606848b8bca6792bdab7d912598d8/FooterContentYanki", {method: 'get'})
+    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/cfcfef95fc759cc4aebc5dcdb646616b/raw/6f06015ac70a70071c350b1156ebbac22ed672d2/FooterContentYanki", {method: 'get'})
     .then(r => r.ok ? r.json() : alert("Error with download"))
     .then(d => dispatch(setContentFooter(d)))
     .catch(e => alert(`Error with download. Type error is: ${e}`))
