8eb03afb33d46f581ec53dc25d9d01cbc12b3488
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-13 17:07:00 +0100

    React app development

--- src/components/Favourite.jsx
+++ src/components/Favourite.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { configureState, clearState } from "../Redux/Favourite/favouriteSlice";
-
+import {scrollToElement} from '../helpers/scroll';
 
 
 export const Favourite = () => {
 
     let dispatch = useDispatch();
+
+    let parentNode = useRef();
 
     const fav = useSelector(state => state.favourite.favourite);
 
@@ -18,15 +20,22 @@ export const Favourite = () => {
         }
     }, [fav, dispatch]);
 
+    useEffect(() => {
+        scrollToElement(parentNode.current);
+    }, []);
+
+
+    const clearLocalStorageAndSlice = () => {
+        localStorage.clear();
+        dispatch(clearState());
+    }
+
     return (
-        <div className="Favourite">
+        <div className="Favourite" ref={parentNode}>
             {
                 fav.map(e => <div key={e.id}>{e.key}</div>)
             }
-            <button onClick={() => {
-                localStorage.clear();
-                dispatch(clearState());
-            }}>clear</button>
+            <button onClick={clearLocalStorageAndSlice}>clear</button>
         </div>
     )
 }
