7527aa2a038c8415ec6516903a6c7b755d67bf71
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-16 15:08:43 +0100

    React app development

--- src/Redux/Catalog/topFilterThunk.js
+++ src/Redux/Catalog/topFilterThunk.js
@@ -1,7 +1,7 @@
 import { setTopTitles } from "./topFilterSlice";
 
 export const topFilterThunk = (dispatch) => {
-    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/b8b0376e4382e04ae67c3ce4d5556c49/raw/36edac0a167d1d65a84a755aa7a834773e605d3e/TopFilterYanki")
+    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/b8b0376e4382e04ae67c3ce4d5556c49/raw/027de3478284f3c8ecfa7387df03d90fc45df6ca/TopFilterYanki")
     .then(res => !res.ok ? alert("Error with download") : res.json())
     .then(data => dispatch(setTopTitles(data)))
     .catch(error => alert(`Error with set data. Type error is: ${error}`))
