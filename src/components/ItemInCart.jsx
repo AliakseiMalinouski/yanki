37c5103b8ed514cb07a14a5bfd8340fdbf23bb8e
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-26 14:11:19 +0200

    helpers added

--- src/components/ItemInCart.jsx
+++ src/components/ItemInCart.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+import { createImageElement } from "../helpers/createImageelement";
 
 export const ItemInCart = React.memo(({price, image, type, translateKey, sizes, color, hover, like, setLanguage, colorOptions}) => {
 
+
+    useEffect(() => {
+        console.log(createImageElement(color, colorOptions))
+    }, [color, colorOptions]);
     
 
     return (
         <div className="ItemInCart">
             <img src={image} alt={setLanguage(`${translateKey}`)} />
             <div className="ItemCartColor">
-
+                
             </div>
             {setLanguage(`${translateKey}`)}
         </div>
---
+++ src/helpers/createImageelement.js
@@ -0,0 +1,6 @@
+export const createImageElement = (flag, array) => {
+    let objectWithParams = array && array.find(elem => elem.alt === flag);
+    for(let key in objectWithParams) {
+        
+    }
+}
