590ac5313c423ea4653b282b89e655d259226ae3
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-29 17:17:17 +0200

    add new helper function

--- src/helpers/getNeededValue.js
+++ src/helpers/getNeededValue.js
@@ -1,8 +1,22 @@
 export const getNeededValue = (value, varWhereWeAreSearching, key) => {
-    if(Array.isArray(varWhereWeAreSearching)) {
-        let objectFromArray = varWhereWeAreSearching.find(element => {
-            return element[key] === value;
-        });
-        return objectFromArray;
-    }
-}
+    // this search request all arguments of function
+      if(Array.isArray(varWhereWeAreSearching)) {
+          let objectFromArray = varWhereWeAreSearching.find(element => {
+              return element[key] === value;
+          });
+          return objectFromArray;
+      }
+      else if(typeof varWhereWeAreSearching === 'object' && !Array.isArray(varWhereWeAreSearching)) { 
+        // this search does not request "value argument"
+          for(let keyOfObject in varWhereWeAreSearching) {
+              if(key === keyOfObject) {
+                  return varWhereWeAreSearching[keyOfObject];
+              }
+          }
+      }
+      else if(typeof varWhereWeAreSearching === 'string') { 
+        // this search does not request "value argument"
+          let neededChar = varWhereWeAreSearching.split(" ").filter(element => element === key).join("");
+          return neededChar;
+      }
+  }
