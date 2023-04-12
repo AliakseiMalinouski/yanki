9912248098579b320daea7d229f158f98860ec9a
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-04-12 18:46:45 +0200

    favourite page development

--- src/components/Favourite.jsx
+++ src/components/Favourite.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useRef } from "react";
 import { configureState, clearState } from "../Redux/Favourite/favouriteSlice";
 import {scrollToElement} from '../helpers/scroll';
-
+import { HintCurrentPage } from "./HintCurrentPage";
+import { useTranslation } from "react-i18next";
 
 export const Favourite = () => {
 
     let dispatch = useDispatch();
 
     let parentNode = useRef();
+
+    let {t} = useTranslation();
 
     const fav = useSelector(state => state.favourite.favourite);
 
@@ -30,11 +33,12 @@ export const Favourite = () => {
         dispatch(clearState());
     }
 
+    let favMemo = useMemo(() => fav.map(e => <div key={e.id}>{e.key}</div>), [fav])
+
     return (
         <div className="Favourite" ref={parentNode}>
-            {
-                fav.map(e => <div key={e.id}>{e.key}</div>)
-            }
+            <HintCurrentPage mainPage={"main-page"} t={t} currentPage={"favourite-page"}/>
+            {favMemo}
             <button onClick={clearLocalStorageAndSlice}>clear</button>
         </div>
     )
