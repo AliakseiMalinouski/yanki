d27cfdfc4f157adf85039bc552c84e8d0b756ba5
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-04-13 19:36:17 +0200

    order history dev

--- src/components/CompletedOrder.jsx
+++ src/components/CompletedOrder.jsx
@@ -16,8 +16,6 @@ export const CompletedOrder = React.memo(({email, items, name, phone, surname, t
             </>
         }
     </div>), [items, setLng, course, currentValute, status])
-
-    console.log(phone)
     
     return (
         <div className="AllInfoAboutOrder">
@@ -26,14 +24,18 @@ export const CompletedOrder = React.memo(({email, items, name, phone, surname, t
             </div>
             <div className="DetailsAboutClientsOrder">
                 <div className="SelfInfo">
-                    <h5>{setLng('name-and-surname')}:  <span>{name}</span></h5>
-                    <p>Email: <span>${email}</span></p>
+                    <h5>{setLng('name-and-surname')}:  <span>{name} {surname}</span></h5>
+                    <p>Email: <span>{email}</span></p>
                     <p>{setLng('phone-number')}: <span>{phone}</span></p>
+                    <p>{setLng('date')}: <span>{dateOptions.date}.{dateOptions.month + 1}.{dateOptions.year}</span></p>
                 </div>
                 <div className="MethodsOfOrder">
                     <h5>{setLng("way-of-delivery")}: <span>{setLng(`post-name`)}</span></h5>
+                    <h5>{setLng(`${delivery}`)}</h5>
                     <p><span>{setLng(`${payMethod}`)}</span></p>
+                    <p>{setLng("address-for-delivery")}: {addressForDelivery}</p>
                 </div>
+                <h4 className="TotalInOrder">{currentValute === 'UAH' ? total + " "+  currentValute : (total / course).toFixed(0) + `${currentValute}`}</h4>
             </div>
         </div>
     )
