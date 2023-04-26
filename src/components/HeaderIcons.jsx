ad85f527a39dd8152d4afef1d3d6c5885e4f51d4
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-04-26 14:02:05 +0200

    result

--- src/components/HeaderIcons.jsx
+++ src/components/HeaderIcons.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { yankiEvents } from "../events";
 
 export const HeaderIcon = ({link, image, alt, favouriteLength, cartLength}) => {
+    const closeModal = () => {
+        yankiEvents.emit("closeModal", true);
+    }
     if(link === '/favourite') {
         return (
-            <NavLink to={link}><img src={image} alt={alt}/><span>{favouriteLength}</span></NavLink>
+            <NavLink onClick={closeModal} to={link}><img src={image} alt={alt}/><span>{favouriteLength}</span></NavLink>
         )
     }
     else if(link === '/cart') {
         return (
-            <NavLink to={link}><img src={image} alt={alt}/><span>{cartLength}</span></NavLink>
+            <NavLink onClick={closeModal} to={link}><img src={image} alt={alt}/><span>{cartLength}</span></NavLink>
         )
     }
     else {
         return (
-            <NavLink to={link}><img src={image} alt={alt}/></NavLink>
+            <NavLink onClick={closeModal} to={link}><img src={image} alt={alt}/></NavLink>
         )
     }
 }
