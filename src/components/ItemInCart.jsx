c4cf3bfbea21c365811e3dfb52f41697d58ccca4
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-04-26 14:26:56 +0200

    testing

--- src/components/ItemInCart.jsx
+++ src/components/ItemInCart.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { createImageElement } from "../helpers/createImageElement";
+// import { createImageElement } from "../helpers/createImageElement";
 import {yankiEvents} from '../events';
 
 export const ItemInCart = React.memo(({price, image, type, translateKey, sizes, color, hover, like, setLanguage, colorOptions, sizesState, item, currentValute, course}) => {
 
     const [imageElement, setImageElement] = useState(null);
 
-    useEffect(() => {
-        let imageElement = createImageElement(color, colorOptions, 'ImageOfColor');
-        setImageElement(imageElement);
-    }, [color, colorOptions]);
+    // useEffect(() => {
+    //     let imageElement = createImageElement(color, colorOptions, 'ImageOfColor');
+    //     setImageElement(imageElement);
+    // }, [color, colorOptions]);
 
     const changeSizesState = () => {
         yankiEvents.emit('changeSizesState', translateKey);
